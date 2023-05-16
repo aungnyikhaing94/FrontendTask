215221b37f9a1d6bf76c5332633bd920f8c46d1e
ank <aungnyikhaing94@gmail.com>
2023-05-16 08:54:14 +0800

    all issues fixed

--- src/app/samplescheduler/events-utc.ts
+++ src/app/samplescheduler/events-utc.ts
@@ -8,315 +8,406 @@ const baseData: any[] = [
       title: "SKU A",
       start: new Date("2023-03-27T09:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 1",
+      isRowSelected: false
     },
     {
       id: 2,
       title: "SKU A",
       start: new Date("2023-03-29T09:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 1",
+      isRowSelected: false
     },
     {
       id: 3,
       title: "SKU C",
       start: new Date("2023-03-31T09:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "1 - 1",
+      isRowSelected: false
+      
     },
     {
       id: 4,
       title: "SKU A",
       start: new Date("2023-03-27T10:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 2",
+      isRowSelected: false
     },
     {
       id: 5,
       title: "SKU A",
       start: new Date("2023-03-29T10:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 2",
+      isRowSelected: false
     },
     {
       id: 6,
       title: "SKU C",
       start: new Date("2023-03-31T10:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "1 - 2",
+      isRowSelected: false
     },
     {
       id: 7,
       title: "SKU D",
       start: new Date("2023-03-27T11:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 3",
+      isRowSelected: false
     },
     {
       id: 8,
       title: "SKU A",
       start: new Date("2023-03-29T11:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 3",
+      isRowSelected: false
     },
     {
       id: 9,
       title: "SKU C",
       start: new Date("2023-03-31T11:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "1 - 3",
+      isRowSelected: false
     },
     {
       id: 10,
       title: "SKU E",
       start: new Date("2023-03-27T12:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 4",
+      isRowSelected: false
     },
     {
       id: 11,
       title: "SKU F",
       start: new Date("2023-03-29T12:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 4",
+      isRowSelected: false
     },
     {
       id: 12,
       title: "SKU C",
       start: new Date("2023-03-31T12:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "1 - 4",
+      isRowSelected: false
     },
     {
       id: 13,
       title: "SKU A",
       start: new Date("2023-03-27T13:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 5",
+      isRowSelected: false
     },
     {
       id: 14,
       title: "SKU A",
       start: new Date("2023-03-29T13:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "1 - 5",
+      isRowSelected: false
     },
     {
       id: 15,
       title: "SKU C",
       start: new Date("2023-03-31T13:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "1 - 5",
+      isRowSelected: false
     },
     {
       id: 16,
       title: "SKU C",
       start: new Date("2023-03-27T14:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "2 - 1",
+      isRowSelected: false
     },
     {
       id: 17,
       title: "SKU A",
       start: new Date("2023-03-28T14:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 1",
+      isRowSelected: false
     },
     {
       id: 18,
       title: "SKU A",
       start: new Date("2023-03-30T14:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 1",
+      isRowSelected: false
     },
     {
       id: 19,
       title: "SKU C",
       start: new Date("2023-03-27T15:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "2 - 2",
+      isRowSelected: false
     },
     {
       id: 20,
       title: "SKU E",
       start: new Date("2023-03-28T15:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 2",
+      isRowSelected: false
     },
     {
       id: 21,
       title: "SKU E",
       start: new Date("2023-03-30T15:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 2",
+      isRowSelected: false
     },
     {
       id: 22,
       title: "SKU C",
       start: new Date("2023-03-27T16:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "2 - 3",
+      isRowSelected: false
     },
     {
       id: 23,
       title: "SKU E",
       start: new Date("2023-03-28T16:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 3",
+      isRowSelected: false
     },
     {
       id: 24,
       title: "SKU E",
       start: new Date("2023-03-30T16:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 3",
+      isRowSelected: false
     },
     {
       id: 25,
       title: "SKU C",
       start: new Date("2023-03-27T17:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "2 - 4",
+      isRowSelected: false
     },
     {
       id: 26,
       title: "SKU D",
       start: new Date("2023-03-28T17:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 4",
+      isRowSelected: false
     },
     {
       id: 27,
       title: "SKU D",
       start: new Date("2023-03-30T17:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 4",
+      isRowSelected: false
     },
     {
       id: 28,
       title: "SKU C",
       start: new Date("2023-03-27T18:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "2 - 5",
+      isRowSelected: false
     },
     {
       id: 29,
       title: "SKU D",
       start: new Date("2023-03-28T18:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 5",
+      isRowSelected: false
     },
     {
       id: 30,
       title: "SKU D",
       start: new Date("2023-03-30T18:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "2 - 5",
+      isRowSelected: false
     },
     {
       id: 31,
       title: "SKU D",
       start: new Date("2023-03-27T19:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 1",
+      isRowSelected: false
     },
     {
       id: 32,
       title: "SKU A",
       start: new Date("2023-03-29T19:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 1",
+      isRowSelected: false
     },
     {
       id: 33,
       title: "SKU C",
       start: new Date("2023-03-31T19:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "3 - 1",
+      isRowSelected: false
     },
     {
       id: 34,
       title: "SKU A",
       start: new Date("2023-03-27T20:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 2",
+      isRowSelected: false
     },
     {
       id: 35,
       title: "SKU A",
       start: new Date("2023-03-29T20:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 2",
+      isRowSelected: false
     },
     {
       id: 36,
       title: "SKU C",
       start: new Date("2023-03-31T20:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "3 - 2",
+      isRowSelected: false
     },
     {
       id: 37,
       title: "SKU D",
       start: new Date("2023-03-27T21:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 3",
+      isRowSelected: false
     },
     {
       id: 38,
       title: "SKU D",
       start: new Date("2023-03-29T21:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 3",
+      isRowSelected: false
     },
     {
       id: 39,
       title: "SKU C",
       start: new Date("2023-03-31T21:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "3 - 3",
+      isRowSelected: false
     },
     {
       id: 40,
       title: "SKU D",
       start: new Date("2023-03-27T22:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 4",
+      isRowSelected: false
     },
     {
       id: 41,
       title: "SKU A",
       start: new Date("2023-03-29T22:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 4",
+      isRowSelected: false
     },
     {
       id: 42,
       title: "SKU C",
       start: new Date("2023-03-31T22:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "3 - 4",
+      isRowSelected: false
     },
     {
       id: 43,
       title: "SKU A",
       start: new Date("2023-03-27T23:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 5",
+      isRowSelected: false
     },
     {
       id: 44,
       title: "SKU A",
       start: new Date("2023-03-29T23:00:00"),
       duration: 2,
-      color: ""
+      color: "",
+      trackID: "3 - 5",
+      isRowSelected: false
     },
     {
       id: 45,
       title: "SKU C",
       start: new Date("2023-03-31T23:00:00"),
       duration: 1,
-      color: ""
+      color: "",
+      trackID: "3 - 5",
+      isRowSelected: false
     }
   ];
 
@@ -334,7 +425,9 @@ export const sampleData = baseData.map(dataItem => (
         start: parseAdjust(dataItem.start), 
         end: parseAdjust(dataItem.end),
         color: dataItem.color,
-        duration: dataItem.duration
+        duration: dataItem.duration,
+        trackId: dataItem.trackID,
+        isRowSelected: dataItem.isRowSelected
     }
 ));
 
--- src/app/samplescheduler/samplescheduler.component.ts
+++ src/app/samplescheduler/samplescheduler.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { ViewChild,ElementRef,Component, ChangeDetectorRef, Renderer2, AfterViewInit } from '@angular/core';
 import {
   SchedulerEvent,
   EventStyleArgs
@@ -11,9 +11,32 @@ import { sampleData } from './events-utc';
   templateUrl: './samplescheduler.component.html',
   styleUrls: ['./samplescheduler.component.css']
 })
-export class SampleschedulerComponent {
+export class SampleschedulerComponent implements AfterViewInit {  
   public selectedDate: Date = new Date("2023-03-27T04:00:00");
   public events: SchedulerEvent[] = sampleData;
+  private trackId: number = 0;
+  private groupId: number= 1;
+  
+  constructor(private renderer: Renderer2,private elementRef: ElementRef){}
+
+  ngAfterViewInit() {
+    const elements: HTMLElement[] = this.elementRef.nativeElement.querySelectorAll('.rowSelector');
+    elements.forEach((element: HTMLElement) => {
+        this.trackId += 1;        
+        
+        let result = `${this.groupId} - ${this.trackId}`;
+
+        if(this.trackId == 5){
+          this.groupId++;
+        }
+
+        if(this.trackId % 5 == 0){
+          this.trackId = 0;
+        }
+
+        element.textContent = result;
+    });
+  }
 
   public get selectedKeys(): number[] {
     return Array.from(this.selectionState);
@@ -35,7 +58,7 @@ export class SampleschedulerComponent {
 
   public getEventStyles = (args: EventStyleArgs) => {
     if(this.isSelected(args.event)){
-      args.event.dataItem.color = "blueviolet";
+      args.event.dataItem.color = "blue";
     } else {
       args.event.dataItem.color = "#ff6358";
     }
@@ -44,9 +67,23 @@ export class SampleschedulerComponent {
 
   public getEventClass = (args: EventStyleArgs) => {
     if(args.event.dataItem.duration > 1){
-      return "column-span-2";
+      return "column-span";
     } else {
       return "";
     }
+  }
+
+  public selectRow(element: any){
+    this.events.forEach( (x: any) => {
+      if(x.trackId == element.textContent){
+        if(x.isRowSelected){
+          this.selectionState.delete(x.id);
+          x.isRowSelected = false;
+        } else {
+          this.selectionState.add(x.id);
+          x.isRowSelected = true;
+        }
+      }
+    });
   }
 }
